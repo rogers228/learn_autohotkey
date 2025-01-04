58c2c353a7a43958ef1391e3430f051b65585e15
rogers228 <61651257+rogers228@users.noreply.github.com>
2025-01-04 15:52:03 +0800

    新增

---
+++ write_read_fileattr/run_target.ahk
@@ -0,0 +1,40 @@
+﻿; 開啟 target_script 的 pyqt5 並隱藏cmd視窗
+; 編譯為 target_script.exe 後
+; 使用 write_attr.py 寫入 ads target_script
+
+python_exe   := "C:\Python310\python.exe" ; 修改為你的 Python 可執行檔完整路徑
+cmd_title    := "start_for_python_pyqt_gui" ; 定義 CMD 視窗的臨時標題
+
+; 讀取本腳本的 ads屬性為target_script的值
+file_path := A_ScriptFullPath
+property_name := "target_script"
+adsPath := file_path ":" property_name
+file := FileOpen(adsPath, "r")
+if file
+{
+    target_script := file.Read() ; 讀取屬性值
+    file.Close()
+}
+else
+{
+    MsgBox, 缺少ads屬性 target_script
+    ExitApp
+}
+
+; 開啟 target_script 並隱藏cmd視窗
+
+if FileExist(target_script)
+{
+    ; MsgBox, %target_script%
+    Run, %ComSpec% /c title %cmd_title% && "%python_exe%" "%target_script%",, Min ; 以最小化方式啟動
+    ; ComSpec ：系統變數，指向 cmd.exe。
+    ; /c      ：執行完命令後自動退出。
+
+    WinWait, %cmd_title%,, 2000 ; 等待指定標題的視窗出現（最多 2 秒）
+    WinHide, %cmd_title% ; 確保cmd視窗隱藏
+}
+else
+{
+    MsgBox, 檔案 "%target_script%" 不存在缺少ads屬性 target_script
+    ExitApp
+}
--- write_read_fileattr/write_attr.py
+++ write_read_fileattr/write_attr.py
@@ -1,20 +1,37 @@
-import win32api
-import win32file
-import win32con
-
 def add_custom_property(file_path, property_name, value):
     # 使用 NTFS Alternate Data Streams 添加屬性
     ads_path = f"{file_path}:{property_name}"
     with open(ads_path, "w", encoding="utf-8") as ads:
         ads.write(value)
 
+def read_custom_property(file_path, property_name):
+    # 使用 NTFS Alternate Data Streams 讀取屬性
+    ads_path = f"{file_path}:{property_name}"
+    try:
+        with open(ads_path, "r", encoding="utf-8") as ads:
+            return ads.read()  # 讀取並返回資料流內容
+    except FileNotFoundError:
+        return None  # 如果資料流不存在，返回 None
+    except Exception as e:
+        raise RuntimeError(f"讀取檔案屬性時發生錯誤: {e}")
+
 def test1():
-    # 使用範例
-    file_path = "test_attr.ahk"
+    # 寫入
+    file_path = "run_target.exe"
     property_name = "target_script"
     value = r"C:\Users\USER\Documents\Learn_python\note\module\03_qt_test\main.py"
     add_custom_property(file_path, property_name, value)
     print(f"已將屬性 '{property_name}' 添加到檔案 '{file_path}'，值為：{value}")
+
+def test2():
+    # 讀取
+    file_path = "run_target.ahk"
+    property_name = "target_script"
+    read_value = read_custom_property(file_path, property_name)
+    if read_value is not None:
+        print(f"檔案 '{file_path}' 的屬性 '{property_name}' 值為：{read_value}")
+    else:
+        print(f"檔案 '{file_path}' 不包含屬性 '{property_name}'")
 
 if __name__ == '__main__':
     test1()
